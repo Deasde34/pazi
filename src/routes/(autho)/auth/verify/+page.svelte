5b3470a8885539d482ea7f12076136218c5fba91
Deasde34 <51971559+Deasde34@users.noreply.github.com>
2023-11-21 21:45:27 -0700

    aaa
    aaa

--- src/routes/(autho)/auth/verify/+page.svelte
+++ src/routes/(autho)/auth/verify/+page.svelte
@@ -22,71 +22,67 @@
     </div>
 </div>
 
-<style lang="scss">
-    // Colors
-    $green: #7ac142;
-    $white: #fff;
+<style lang="css">
+/* Colors */
+:root {
+    --green: #7ac142;
+    --white: #fff;
+}
 
-    // Misc
-    $curve: cubic-bezier(0.65, 0, 0.45, 1);
+/* Misc */
+:root {
+    --curve: cubic-bezier(0.65, 0, 0.45, 1);
+}
 
-    body {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+
+.checkmark {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    display: block;
+    stroke-width: 2;
+    stroke: var(--white);
+    stroke-miterlimit: 10;
+    box-shadow: inset 0px 0px 0px var(--green);
+    animation: fill 0.4s ease-in-out 0.4s forwards, scale 0.3s ease-in-out 0.9s both;
+}
+
+.checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke-miterlimit: 10;
+    stroke: var(--green);
+    fill: none;
+    animation: stroke 0.6s var(--curve) forwards;
+}
+
+.checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s var(--curve) 0.8s forwards;
+}
+
+@keyframes stroke {
+    100% {
+        stroke-dashoffset: 0;
     }
+}
 
-    .checkmark {
-        width: 56px;
-        height: 56px;
-        border-radius: 50%;
-        display: block;
-        stroke-width: 2;
-        stroke: $white;
-        stroke-miterlimit: 10;
-        box-shadow: inset 0px 0px 0px $green;
-        animation: fill 0.4s ease-in-out 0.4s forwards,
-            scale 0.3s ease-in-out 0.9s both;
+@keyframes scale {
+    0%, 100% {
+        transform: none;
     }
+    50% {
+        transform: scale3d(1.1, 1.1, 1);
+    }
+}
 
-    .checkmark__circle {
-        stroke-dasharray: 166;
-        stroke-dashoffset: 166;
-        stroke-width: 2;
-        stroke-miterlimit: 10;
-        stroke: $green;
-        fill: none;
-        animation: stroke 0.6s $curve forwards;
+@keyframes fill {
+    100% {
+        box-shadow: inset 0px 0px 0px 30px var(--green);
     }
+}
 
-    .checkmark__check {
-        transform-origin: 50% 50%;
-        stroke-dasharray: 48;
-        stroke-dashoffset: 48;
-        animation: stroke 0.3s $curve 0.8s forwards;
-    }
-
-    @keyframes stroke {
-        100% {
-            stroke-dashoffset: 0;
-        }
-    }
-
-    @keyframes scale {
-        0%,
-        100% {
-            transform: none;
-        }
-        50% {
-            transform: scale3d(1.1, 1.1, 1);
-        }
-    }
-
-    @keyframes fill {
-        100% {
-            box-shadow: inset 0px 0px 0px 30px $green;
-        }
-    }
 </style>
